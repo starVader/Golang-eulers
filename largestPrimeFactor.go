babb21ad9155bc413e5327dd1236d51786a849ba
Rakesh Raushan <starvader473@gmail.com>
2019-06-17 19:45:11 +0530

    Update largestPrimeFactor.go

--- largestPrimeFactor.go
+++ largestPrimeFactor.go
@@ -15,7 +15,7 @@ func Find(A ,B ,C int) int {
 		fmt.Println(A,B)
 		if B  > C { //If the current Prime is greater than last prime (C) is greater  
 			C = B // Replace the current greatest integer with the current one
-			A = A/B // reduce A to half
+			A = A/B // factorize A
 			//fmt.Printf("A %d B %d C %d",A,B,C)
 		}
 		B = 2 //reset B to 2 and start again
